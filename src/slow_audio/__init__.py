d186ae51f4dfd099767312ca7a947dc46cb28397
Connor Finley <connor.finley5@gmail.com>
2020-01-03 14:06:31 -0500

    Initial Commit

---
+++ src/slow_audio/__init__.py
@@ -0,0 +1,14 @@
+def initialize_addon():
+    pass
+
+
+initialize_addon()
+
+"""
+default_command = anki.sound.mplayerCmd
+anki.sound.stopMplayer()
+anki.sound.mplayerCmd.pop()
+#anki.sound.mplayerCmd.append("-speed")
+anki.sound.mplayerCmd.append("0.7")
+print(anki.sound.mplayerCmd)
+"""
